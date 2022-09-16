19bc19e8ee6038a2d1b7bde47b902433ad093176
florent etheve <fetheve2@gmail.com>
2022-09-16 10:35:12 +0400

    node memory

--- app.ts
+++ app.ts
@@ -6,3 +6,5 @@ import routerhandler from "./Core/Routes/Routes";
 routerhandler.make();
 ConfigServer.start()
 
+console.log("info usage mémoire : ") 
+const used = process.memoryUsage().heapUsed / 1024 / 1024; console.log(`The script uses approximately ${used} MB`);
