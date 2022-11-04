380480814197f476e5b4696006d00ed981809c14
florent etheve <fetheve2@gmail.com>
2022-11-04 11:12:49 +0400

    node

--- Core/services/Server.ts
+++ Core/services/Server.ts
@@ -100,10 +100,7 @@ console.log("server init !")
     public static start() {
           console.log("info usage mémoire : ") 
         const used = process.memoryUsage().heapUsed / 1024 / 1024; console.log(`The script uses approximately ${used} MB`);
-        app.get("/", (req, res) => {
-            console.log("sending vuejs")
-            res.sendFile(path.join(__dirname, "client/dist/index.html"));
-          });
+ 
         this.getInstance().init();
     }
 
